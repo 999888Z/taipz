6c5941f72b5342d7a3ffd82f550aabf820e44a0a
John <johnwilday@yahoo.com>
2023-05-21 13:42:17 -0600

    Built Top Artist sidebar

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -7,7 +7,7 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const TopArtists = () => {
     
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    
     const { data, isFetching, error } = useGetTopChartsQuery();
 
 
@@ -24,7 +24,7 @@ return (
 
 <div className='flex flex-col'>
     <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'
-    >Discover Top Charts 
+    >Top Artists 
     </h2>
 
 <div className='flex flex-wrap sm:justify-start justify=center gap-8'>
