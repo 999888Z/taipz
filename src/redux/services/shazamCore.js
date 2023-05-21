c3de4a426d65b311b920ce03e5509402d2b724c0
John <johnwilday@yahoo.com>
2023-05-21 14:53:51 -0600

    Built genre choosing functionality

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -19,6 +19,7 @@ baseQuery: fetchBaseQuery({
 }),
 endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
     getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
     getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
@@ -30,6 +31,7 @@ endpoints: (builder) => ({
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
