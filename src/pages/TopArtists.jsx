009fd541e7808a0eb58861eeb3c60b131d1dd93e
John <johnwilday@yahoo.com>
2023-05-21 13:53:19 -0600

    Built ArtistCard in TopArtists

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -28,15 +28,8 @@ return (
     </h2>
 
 <div className='flex flex-wrap sm:justify-start justify=center gap-8'>
-        {data?.map((song,i) => (
-            <SongCard
-                key={song.key}
-                song={song}
-                isPlaying={isPlaying}
-                activeSong={activeSong}
-                data={data}
-                i={i}
-            />
+        {data?.map((track) => (
+            <ArtistCard key={track.key} track={track}/>
 
 
 
