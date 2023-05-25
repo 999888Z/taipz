387964d373e447543582c726a49aa8b1349d74fb
Bryan W <bhoff1980@gmail.com>
2023-05-25 03:02:19 -0600

    all schemas setup for initial download attempt

---
+++ server/models/songModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const SongSchema = mongoose.Schema({
+  songName: {
+    type: String,
+    required: true,
+  },
+  audioLink: {
+    type: String,
+    required: true,
+  },
+  track: {
+    type: Number,
+    required: true,
+  },
+
+  // shows: [showsSchema],
+
+  timeStamps: true,
+});
+
+const Song = mongoose.model("Song", ShowSchema);
+
+export default Song;
